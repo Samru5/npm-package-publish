043c8e9349712ccda506c1bd2f0fed9a86ff0048
Samru5 <48197286+Samru5@users.noreply.github.com>
2020-06-16 18:42:35 +0530

    using published module/package in code

---
+++ npm calculator output/node_modules/npm-calculator-example-js/index.js
@@ -0,0 +1,21 @@
+module.exports={
+    addition:function(number1,number2){
+        return number1+number2;
+    },
+
+    subtraction:function(number1,number2){
+        return number1-number2;
+    },
+
+    multiplication:function(number1,number2){
+        return number1*number2;
+    },
+
+    division:function(number1,number2){
+        return number1/number2;
+    },
+
+    modulus:function(number1,number2){
+        return number1%number2;
+    }
+};
---
+++ npm calculator output/output.js
@@ -0,0 +1,16 @@
+var app =require('npm-calculator-example-js');
+
+var add=app.addition(5,3);
+console.log("Addition is-:"+add)
+
+var subtract=app.subtraction(40,5)
+console.log("Subtraction is-:"+subtract)
+
+var multiply=app.multiplication(8,4)
+console.log("Multiplication is-:"+multiply)
+
+var divide=app.division(18,3)
+console.log("Division is-:"+divide)
+
+var modulus=app.modulus(10,4)
+console.log("Modulus is-:"+modulus)
